f8c92d1e488d67b98123d96319e3e9a1f0c78715
JulioX1047 <julioxmenezes@gmail.com>
2024-09-16 17:26:30 -0300

    Add files via upload
    ATIVIDADES AULA 02 (PYTON) 16/09/2024

---
+++ atv01.py
@@ -0,0 +1,10 @@
+id1 = int(input("Informe a primeira idade "))
+id2 = int(input("Informe a segunda idade "))
+
+if id1>id2:
+    print(f"{id1} é maior que a {id2} ")
+elif id1<id2:
+   print(f"{id1} É menor que {id2}")
+elif id1==id2:
+    print(f"{id1} é igual a {id2}")
+
---
+++ atv02.py
@@ -0,0 +1,6 @@
+palavra01 = input("Digite uma palavra")
+palavra02 = input("Digite uma palavra")
+if palavra01==palavra02:
+    print("são iguais ")
+else:
+    print("Não são palavras iguais")
